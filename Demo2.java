aec22aaa23cadafea261e4b2cb84d681c250d15d
Hemitha123 <hemithatech@gmail.com>
2025-09-04 21:53:01 +0530

    add changes

--- Demo2.java
+++ Demo2.java
@@ -1,17 +1,24 @@
+// public class Demo2 {
+//     public static void main(String[] args) {
+//         int age = 48;
+//         System.out.println("Age: " + age);
+//         double price = 45.50;
+//         int a =10;
+//         int b = 20;
+//         System.out.println("Sum of a and b: " + (a + b));
+//         System.out.println("Value of b: " + b);
+//         String name = "peter";
+//         System.out.println(name);
+//         System.out.println("Price: " + price);
+//     }
+// }
+// variables, data types, basic operations
 public class Demo2 {
     public static void main(String[] args) {
-        int age = 48;
-        System.out.println("Age: " + age);
-        double price = 45.50;
-        int a =10;
+       int a = 10;
         int b = 20;
-        System.out.println("Sum of a and b: " + (a + b));
-        System.out.println("Value of b: " + b);
-        String name = "peter";
-        System.out.println(name);
-        System.out.println("Price: " + price);
-        
-        
-
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
+
