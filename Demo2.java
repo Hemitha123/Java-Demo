02a8bbba8a4817a5d4cf9e65096baa00343cd51e
Hemitha123 <hemithatech@gmail.com>
2025-09-04 21:24:59 +0530

    add changes

---
+++ Demo2.java
@@ -0,0 +1,18 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        int age = 48;
+        System.out.println("Age: " + age);
+        double price = 45.50;
+        int a =10;
+        int b = 25;
+
+        b = 20;
+        System.out.println("Value of b: " + b);
+        String name = "peter";
+        System.out.println(name);
+        System.out.println("Price: " + price);
+        
+        
+
+    }
+}
