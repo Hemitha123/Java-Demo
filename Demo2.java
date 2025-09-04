8b53ab230f0b73a4207acac1829af993510c29a3
Hemitha123 <hemithatech@gmail.com>
2025-09-04 21:28:10 +0530

    add changes

--- Demo2.java
+++ Demo2.java
@@ -4,9 +4,8 @@ public class Demo2 {
         System.out.println("Age: " + age);
         double price = 45.50;
         int a =10;
-        int b = 25;
-
-        b = 20;
+        int b = 20;
+        System.out.println("Sum of a and b: " + (a + b));
         System.out.println("Value of b: " + b);
         String name = "peter";
         System.out.println(name);
