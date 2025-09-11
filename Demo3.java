c6dfe26f8c06ca4c1acd9d320f553ac6b286a176
Hemitha123 <hemithatech@gmail.com>
2025-09-11 22:15:42 +0530

    add changes

--- Demo3.java
+++ Demo3.java
@@ -5,6 +5,6 @@ public class Demo3 {
     public static void main(String[] args) {
        Scanner sc =new Scanner(System.in);
        String name = sc.nextLine();
-         System.out.println(name);
+         System.out.print(name);
     }
 }
